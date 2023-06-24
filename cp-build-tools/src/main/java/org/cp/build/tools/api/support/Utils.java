767bbd951a177ed74c70ea0c8140f3b8e9074860
John Blum <jblum@vmware.com>
2023-06-23 21:50:01 -0700

    Add PERCENT constant to Utils.

--- cp-build-tools/src/main/java/org/cp/build/tools/api/support/Utils.java
+++ cp-build-tools/src/main/java/org/cp/build/tools/api/support/Utils.java
@@ -48,6 +48,7 @@ public abstract class Utils {
   public static final String EMPTY_STRING = "";
   public static final String FORWARD_SLASH = "/";
   public static final String LINE_SEPARATOR = System.getProperty("line.separator");
+  public static final String PERCENT = "%";
   public static final String SINGLE_SPACE = " ";
   public static final String TAB = "\t";
   public static final String NEW_LINE_REGEX = "\\n";
