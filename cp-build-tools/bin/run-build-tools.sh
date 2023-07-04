8fdc5ad184d9872966b87691fde45b665763bfc3
John Blum <jblum@vmware.com>
2023-07-04 13:25:19 -0700

    Change Codeprimate Build Maven project artifactId to maven-build.
    Change Codeprimate Build Tools Maven project artifactId to build-tools.
    
    Edit Codeprimate Build Maven project description.

--- cp-build-tools/bin/run-build-tools.sh
+++ cp-build-tools/bin/run-build-tools.sh
@@ -2,4 +2,4 @@
 
 export CP_BUILD_TOOLS_HOME=${CP_BUILD_TOOLS_HOME:-/Users/jblum/cpdev/Codeprimate/workspaces/cp-core-workspace/cp-build/cp-build-tools}
 
-java -jar ${CP_BUILD_TOOLS_HOME}/target/cp-build-tools-2.0.0-SNAPSHOT.jar
+java -jar ${CP_BUILD_TOOLS_HOME}/target/build-tools-2.0.0-SNAPSHOT.jar
