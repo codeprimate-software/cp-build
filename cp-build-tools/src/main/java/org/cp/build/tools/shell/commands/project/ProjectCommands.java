3aaa036e2af7f8204465cc1b980702c5c9c597bc
John Blum <jblum0@gmail.com>
2023-07-15 23:14:58 -0700

    Add 'project recent' command to ProjectCommands.
    Apply Utils newlines to project commands.
    
    Refactor 'project use' command to use recent projects in addition to loaded projects.

--- cp-build-tools/src/main/java/org/cp/build/tools/shell/commands/project/ProjectCommands.java
+++ cp-build-tools/src/main/java/org/cp/build/tools/shell/commands/project/ProjectCommands.java
@@ -18,10 +18,12 @@ package org.cp.build.tools.shell.commands.project;
 import java.io.File;
 import java.io.FileFilter;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import org.cp.build.tools.api.model.Project;
 import org.cp.build.tools.api.service.ProjectManager;
+import org.cp.build.tools.api.service.ProjectManager.RecentProject;
 import org.cp.build.tools.api.support.Utils;
 import org.cp.build.tools.maven.model.MavenProject;
 import org.cp.build.tools.shell.commands.AbstractCommandsSupport;
@@ -72,25 +74,28 @@ public class ProjectCommands extends AbstractCommandsSupport {
   public String describe() {
 
     return currentProject()
-      .map(project -> "Name: ".concat(project.getName()).concat(Utils.newLine())
-        .concat("Description: ").concat(indent(project.getDescription())).concat(Utils.newLine())
-        .concat("Version: ").concat(project.getVersion().toString()).concat(Utils.newLine())
-        .concat("Artifact: ").concat(project.getArtifact().toString()).concat(Utils.newLine())
+      .map(project -> "Name: ".concat(Utils.newLineAfter(project.getName()))
+        .concat("Description: ").concat(Utils.newLineAfter(indent(project.getDescription())))
+        .concat("Version: ").concat(Utils.newLineAfter(project.getVersion().toString()))
+        .concat("Artifact: ").concat(Utils.newLineAfter(project.getArtifact().toString()))
         .concat(Utils.newLine())
-        .concat("Licenses: ").concat(project.getLicenses().toString()).concat(Utils.newLine())
-        .concat("Organization: ").concat(Utils.nullSafeToString(project.getOrganization()).concat(Utils.newLine())
-        .concat("Developers: ").concat(project.getDevelopers().toString()).concat(Utils.newLine()))
+        .concat("Licenses: ").concat(Utils.newLineAfter(project.getLicenses().toString()))
+        .concat("Organization: ").concat(Utils.newLineAfter(Utils.nullSafeToString(project.getOrganization())))
+        .concat("Developers: ").concat(Utils.newLineAfter(project.getDevelopers().toString()))
+
         .concat(Utils.newLine())
-        .concat("Source Repository: ").concat(project.getSourceRepository().toString()).concat(Utils.newLine())
-        .concat("Issue Tracker: ").concat(project.getIssueTracker().toString()).concat(Utils.newLine())
+        .concat("Source Repository: ").concat(Utils.newLineAfter(project.getSourceRepository().toString()))
+        .concat("Issue Tracker: ").concat(Utils.newLineAfter(project.getIssueTracker().toString()))
       )
       .orElseThrow(() -> new IllegalStateException("Project was not set"));
   }
 
-  @Command(command = "list", description = "List all available projects")
+  @Command(command = "list", description = "List all loaded projects")
   public String list() {
 
-    int length = projects().stream()
+    List<Project> projects = projects();
+
+    int length = projects.stream()
       .map(Project::getName)
       .map(String::length)
       .reduce(Math::max)
@@ -98,7 +103,7 @@ public class ProjectCommands extends AbstractCommandsSupport {
 
     StringBuilder output = new StringBuilder();
 
-    for (Project project : projects()) {
+    for (Project project : projects) {
 
       String projectName = project.getName();
 
@@ -107,7 +112,8 @@ public class ProjectCommands extends AbstractCommandsSupport {
       String resolvedProjectName = isCurrentProject(project) ? "[*] ".concat(formattedProjectName)
         : "[ ] ".concat(formattedProjectName);
 
-      output.append(resolvedProjectName).append(" | ").append(project.getDirectory()).append(Utils.newLine());
+      output.append(resolvedProjectName).append(" | ")
+        .append(Utils.newLineAfter(project.getDirectory().getAbsolutePath()));
     }
 
     return output.toString();
@@ -123,13 +129,46 @@ public class ProjectCommands extends AbstractCommandsSupport {
     return String.format("Project set to [%s]", project);
   }
 
+  @Command(command = "recent", description = "List all recent projects")
+  public String recent() {
+
+    List<ProjectManager.RecentProject> recentProjects = recentProjects();
+
+    int length = recentProjects.stream()
+      .map(ProjectManager.RecentProject::getName)
+      .map(String::length)
+      .reduce(Math::max)
+      .orElse(25);
+
+    StringBuilder output = new StringBuilder();
+
+    for (ProjectManager.RecentProject recentProject : recentProjects) {
+
+      String recentProjectName = recentProject.getName();
+      String formattedRecentProjectName = Utils.nullSafeFormatString(recentProjectName, length);
+
+      output.append(formattedRecentProjectName).append(" | ")
+        .append(Utils.newLineAfter(recentProject.getLocation().getAbsolutePath()));
+    }
+
+    return output.toString();
+  }
+
   @Command(command = "use", description = "Sets current project to the given name")
   public String use(@NonNull String projectName) {
 
-    return getProjectManager().findByName(projectName)
-      .map(getProjectManager()::setCurrentProject)
+    ProjectManager projectManager = getProjectManager();
+
+    return projectManager.findProjectByName(projectName)
+      .map(projectManager::setCurrentProject)
       .map(project -> String.format("Project set to [%s]", project))
-      .orElseGet(() -> String.format("Project with name [%s] not found", projectName));
+      .orElseGet(() -> projectManager.findRecentProjectByName(projectName)
+        .filter(RecentProject::exists)
+        .map(RecentProject::getLocation)
+        .map(projectManager::resolveByLocation)
+        .map(projectManager::setCurrentProject)
+        .map(project -> String.format("Project set to [%s]", project))
+        .orElseGet(() -> String.format("Project with name [%s] not found", projectName)));
   }
 
   @NonNull @Bean
