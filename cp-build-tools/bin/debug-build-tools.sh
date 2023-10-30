5d7297911622499927654930ded654fcc89dd777
John Blum <jblum0@gmail.com>
2023-10-30 12:17:21 -0700

    Correct filesystem path locations in Bash shell scripts.

--- cp-build-tools/bin/debug-build-tools.sh
+++ cp-build-tools/bin/debug-build-tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export CP_BUILD_TOOLS_HOME=${CP_BUILD_TOOLS_HOME:-/Users/jblum/cpdev/Codeprimate/workspaces/cp-core-workspace/cp-build/cp-build-tools}
+export CP_BUILD_TOOLS_HOME=${CP_BUILD_TOOLS_HOME:-/Users/jblum/cpdev/Codeprimate/projects/cp-core-workspace/cp-build/cp-build-tools}
 
 java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar \
   ${CP_BUILD_TOOLS_HOME}/target/build-tools-2.0.0-SNAPSHOT.jar
