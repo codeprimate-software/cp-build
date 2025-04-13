41c96e1885be8d0c264c05d75f0264b4acf888af
John Blum <jblum0@gmail.com>
2025-04-13 15:30:15 -0700

    Change Bash shell scripts to refer to Codeprimate Build Tools 3.0.0-SNAPSHOT.

--- cp-build-tools/bin/debug-build-tools.sh
+++ cp-build-tools/bin/debug-build-tools.sh
@@ -2,4 +2,4 @@
 export CP_BUILD_TOOLS_HOME=${CP_BUILD_TOOLS_HOME:-/Users/jblum/cpdev/Codeprimate/projects/cp-core-workspace/cp-build/cp-build-tools}
 
 java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=5005,suspend=n -jar \
-  ${CP_BUILD_TOOLS_HOME}/target/build-tools-2.0.0-SNAPSHOT.jar
+  ${CP_BUILD_TOOLS_HOME}/target/build-tools-3.0.0-SNAPSHOT.jar
--- cp-build-tools/bin/run-build-tools.sh
+++ cp-build-tools/bin/run-build-tools.sh
@@ -2,4 +2,4 @@
 
 export CP_BUILD_TOOLS_HOME=${CP_BUILD_TOOLS_HOME:-$HOME/cpdev/Codeprimate/projects/cp-core-workspace/cp-build/cp-build-tools}
 
-java -jar ${CP_BUILD_TOOLS_HOME}/target/build-tools-2.0.0-SNAPSHOT.jar
+java -jar ${CP_BUILD_TOOLS_HOME}/target/build-tools-3.0.0-SNAPSHOT.jar
